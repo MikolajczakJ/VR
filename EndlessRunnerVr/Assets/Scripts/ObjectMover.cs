bb503a0c256071ec1bd2659ac370e24a579372ea
mikolajczakj <jakubmikolajczak1ti@gmail.com>
2024-11-25 14:03:40 +0100

    moving obstacles

--- EndlessRunnerVr/Assets/Scripts/ObjectMover.cs
+++ EndlessRunnerVr/Assets/Scripts/ObjectMover.cs
@@ -5,9 +5,38 @@ using UnityEngine;
 public class ObjectMover : MonoBehaviour
 {
     [SerializeField] Vector3 speedVector;
+    [SerializeField] bool moveHorizontally;
+    [SerializeField] private float horizontalRange = 5f; 
+    [SerializeField] private float horizontalSpeed = 2f; 
+
+    private bool movingRight = true; 
+    private Vector3 startPosition;  
     // Update is called once per frame
     void Update()
     {
         transform.Translate(speedVector * Time.deltaTime);
+        if (moveHorizontally)
+        {
+            HorizontalMovement();
+        }
+    }
+
+    private void HorizontalMovement()
+    {
+        float currentZ = transform.position.z;
+        float leftBoundary = startPosition.z - horizontalRange;
+        float rightBoundary = startPosition.z + horizontalRange;
+
+        if (currentZ <= leftBoundary)
+        {
+            movingRight = true; 
+        }
+        else if (currentZ >= rightBoundary)
+        {
+            movingRight = false; 
+        }
+
+        float horizontalDirection = movingRight ? 1 : -1;
+        transform.Translate(Vector3.forward * horizontalDirection * horizontalSpeed * Time.deltaTime);
     }
 }
