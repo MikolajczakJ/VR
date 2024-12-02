4a633b04e95b0a15d8445f86114daf1d810d883b
WojMulkowski <w.mulkowski44@gmail.com>
2024-12-02 23:06:17 +0100

    Display red wall and text GAME OVER with score

--- EndlessRunnerVr/Assets/Scripts/GameManager.cs
+++ EndlessRunnerVr/Assets/Scripts/GameManager.cs
@@ -1,23 +1,36 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    [SerializeField] float score;
-    [SerializeField] float spawnInterval;
-    [SerializeField] byte lifes;
-    [SerializeField] Spawner spawner;
+
+    [SerializeField] private float score;
+    [SerializeField] private float spawnInterval;
+    [SerializeField] private byte lifes;
+    [SerializeField] private Spawner spawner;
+    [SerializeField] private GameObject redWall;
+    [SerializeField] private TextMeshProUGUI gameOverText; // Dla TextMeshPro
+
+
     public float Score { get => score; }
     public float Lifes { get => lifes; }
 
-    // Start is called before the first frame update
     void Start()
     {
         Instance = this;
+        if (redWall != null)
+        {
+            redWall.SetActive(false);
+        }
+        if (gameOverText != null)
+        {
+            gameOverText.gameObject.SetActive(false);
+        }
         StartCoroutine(SpawnRoutine());
     }
+
     IEnumerator SpawnRoutine()
     {
         if (spawner == null)
@@ -25,25 +38,50 @@ public class GameManager : MonoBehaviour
             yield return null;
             yield break;
         }
-        while (true)
+        while (lifes > 0)
         {
             spawner.Spawn();
             yield return new WaitForSeconds(spawnInterval);
         }
     }
-        // Update is called once per frame
+
     void Update()
     {
-          score += Time.deltaTime;
+        score += Time.deltaTime;
     }
+
     public void ReduceLife()
     {
         lifes--;
+        Debug.Log("Lives left: " + lifes);
+
+        if (lifes <= 0)
+        {
+            lifes = 0;
+            Debug.Log("Game Over! Activating red wall.");
+            if (redWall != null)
+            {
+                redWall.SetActive(true);
+            }
+            if (gameOverText != null)
+            {
+                gameOverText.text = "GAME OVER\nScore: " + Mathf.FloorToInt(score);
+                gameOverText.gameObject.SetActive(true);
+                Debug.Log("Game Over text activated!");
+            }
+            else
+            {
+                Debug.LogError("Game Over Text is not assigned!");
+            }
+            Time.timeScale = 0; // Zatrzymanie gry
+        }
     }
+
     public void CoinPick()
     {
         score += 10;
     }
+
     public void PillPick()
     {
         lifes++;
